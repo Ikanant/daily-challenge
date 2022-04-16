94253018effec6e9137806bd69807ca6cca9daad
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2022-04-16 11:41:36 -0400

    Quick and somewhat dirty solution for the Merge Two Sorted List challenge

---
+++ Linked-Lists/Merge-Two-Sorted-Lists.cs
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    // SOLUTION ONE - DIRTY
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null) return null;
+        
+        ListNode rootNode = new ListNode();
+        ListNode indexNode = null;
+        
+        // [1,2,4]
+        // [1,3,4]
+        while (list1 != null || list2 != null) {
+            if (indexNode == null) {
+                indexNode = rootNode;
+            }
+            else {
+                indexNode.next = new ListNode();
+                indexNode = indexNode.next;
+            }
+            
+            if (list1 == null) {
+                indexNode.val = list2.val;
+                list2 = list2.next;
+            }
+            else if (list2 == null) {
+                indexNode.val = list1.val;
+                list1 = list1.next;
+            }
+            else {
+                if (list1.val <= list2.val) {
+                    indexNode.val = list1.val;
+                    list1 = list1.next;
+                }
+                else {
+                    indexNode.val = list2.val;
+                    list2 = list2.next;
+                }
+            }
+        }
+        
+        return rootNode;
+    }
+}
