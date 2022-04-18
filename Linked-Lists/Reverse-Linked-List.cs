23327efe74966394a100be9f51ed43a20694a7c0
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2022-04-18 09:29:51 -0400

    Embarassing WAY cleaner solution to reverse a basic linked list

--- Linked-Lists/Reverse-Linked-List.cs
+++ Linked-Lists/Reverse-Linked-List.cs
@@ -11,30 +11,48 @@
  */
 public class Solution {
     // -----------------------------------------------------------------------------------------------------
-    // Solution ONE - DIRTY  -------------------------------------------------------------------------------------
+    // Solution TWO - CLEANER  -----------------------------------------------------------------------------
     // -----------------------------------------------------------------------------------------------------
     public ListNode ReverseList(ListNode head) {
-        if (head == null) return null;
-
-        ListNode index = head;
-        ListNode newRoot = head;
+        // 1 2 3 4
+        // 4 3 2 1
         ListNode prevNode = null;
-
-        while (head.next != null) {
-            if (index.next.next == null) {
-                newRoot = index.next;
-                newRoot.next = index;
-                index = head;
-            } else if (index.next.next == index) {
-                index.next = prevNode;
-                index = head;
-                prevNode = null;
-            } else {
-                prevNode = index;
-                index = index.next;
-            }
+        ListNode indexNode = head;
+        ListNode tempNode = null;
+        while (indexNode != null) {
+            tempNode = indexNode.next;
+            indexNode.next = prevNode;
+            prevNode = indexNode;
+            indexNode = tempNode;
         }
-
-        return newRoot;
+        return prevNode;
     }
+
+    // -----------------------------------------------------------------------------------------------------
+    // Solution ONE - DIRTY  -------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------
+    // public ListNode ReverseList(ListNode head) {
+    //     if (head == null) return null;
+
+    //     ListNode index = head;
+    //     ListNode newRoot = head;
+    //     ListNode prevNode = null;
+
+    //     while (head.next != null) {
+    //         if (index.next.next == null) {
+    //             newRoot = index.next;
+    //             newRoot.next = index;
+    //             index = head;
+    //         } else if (index.next.next == index) {
+    //             index.next = prevNode;
+    //             index = head;
+    //             prevNode = null;
+    //         } else {
+    //             prevNode = index;
+    //             index = index.next;
+    //         }
+    //     }
+
+    //     return newRoot;
+    // }
 }
